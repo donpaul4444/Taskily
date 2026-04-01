a84413628ead85fa38e115daf0c6179fcb1f76eb
Don Paul <donpaul4444@gmail.com>
2026-04-01 20:51:11 +0530

    login page started

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="flex justify-center w-full h-screen">
+      <div className="bg-[#F9FAFB] w-1/2 justify-center flex items-center">
+        <div className="border-4 border-[#1D5746]  w-10 h-10 flex justify-center items-center rounded-full">
+          <div className="bg-[#1D5746]  w-3 h-3 rounded-full"></div>
+        </div>
+      </div>
+      <div className="bg-[#1D5746] w-1/2">Right</div>
+    </div>
+  );
+};
+
+export default Login;
